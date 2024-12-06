9dff4d336a265ab33a390264c7ccbf716eff2c30
John Berthels <john.berthels@gmail.com>
2024-12-06 08:21:53 +0000

    day 6 part 2 - brute force (74s)

--- d6.py
+++ d6.py
@@ -18,13 +18,40 @@ def parse(lines: list[str]) -> tuple[pt, set[pt]]:
     return guard, set(obs)
 
 
+def p2(lines: list[str]) -> int:
+    print('p2')
+    guard, obs = parse(lines)
+    # p1 took 5344 steps, but we may get longer paths
+    max_steps = 130 * 130   # 16900
+    loopers = set()
+    for j, line in enumerate(lines):
+        print(f'j {j}')
+        for i, c in enumerate(line):
+            o = pt(i, j)
+            if o in obs or o == guard:
+                continue
+            oset = obs.copy()
+            oset.add(o)
+            visited, steps = simulate(guard, oset, max_steps)
+            if steps == max_steps:
+                loopers.add(o)
+
+    return len(loopers)
+
+
 def p1(lines: list[str]) -> int:
     guard, obs = parse(lines)
-#    print(guard)
-#    print(obs)
+    visited, steps = simulate(guard, obs)
+    return len(visited)
+
+
+def simulate(guard: pt, obs: set[pt], max_steps=None) -> tuple[set[pt], int]:
+    #    print(guard)
+    #    print(obs)
     dirs = NESW
     dir = 0
     visited = set()
+    steps = 0
     while guard.within(lines):
         visited.add(guard)
         next = guard.add(dirs[dir])
@@ -32,11 +59,17 @@ def p1(lines: list[str]) -> int:
             dir = (dir + 1) % len(dirs)
         else:
             guard = next
+        steps += 1
+        if max_steps is not None:
+            if steps >= max_steps:
+                break
 
-    return len(visited)
+#    print(f'steps {steps}')
+    return visited, steps
 
 
 if __name__ == "__main__":
     d = Day(6)
     lines = d.read_lines()
     print(p1(lines))
+    print(p2(lines))
