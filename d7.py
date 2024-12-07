8cd06179ed7edc852783617cca45a3d16ed0b032
John Berthels <john.berthels@gmail.com>
2024-12-07 06:31:11 +0000

    day 7 part 2

--- d7.py
+++ d7.py
@@ -2,11 +2,18 @@ from day import Day, str_to_nums
 from itertools import zip_longest
 
 
-def ops(n) -> list[list[str]]:
+def bin_ops(n) -> list[list[str]]:
     if n <= 0:
         return [[]]
-    ll = ops(n-1)
+    ll = bin_ops(n-1)
     return [l[:] + ['+'] for l in ll] + [l[:] + ['*'] for l in ll]
+
+
+def tri_ops(n) -> list[list[str]]:
+    if n <= 0:
+        return [[]]
+    ll = tri_ops(n-1)
+    return [l[:] + ['+'] for l in ll] + [l[:] + ['*'] for l in ll] + [l[:] + ['||'] for l in ll]
 
 
 def parse(lines) -> list[tuple[int, list[int]]]:
@@ -24,21 +31,35 @@ def eval(nums: list[int], ops: list[str]) -> int:
             total += n
         elif op == '*':
             total *= n
+        elif op == '||':
+            total = int(str(total) + str(n))
         else:
             raise RuntimeError(f'bad op {op}')
     return total
 
 
-def is_true(eq) -> bool:
+def is_true_p1(eq) -> bool:
     total, nums = eq
-    return total in [eval(nums, op) for op in ops(len(nums)-1)]
+    return total in [eval(nums, op) for op in bin_ops(len(nums)-1)]
+
+
+def is_true_p2(eq) -> bool:
+    #    print(eq)
+    total, nums = eq
+    return total in [eval(nums, op) for op in tri_ops(len(nums)-1)]
+
+
+def p2(lines: list[str]) -> int:
+    eqs = parse(lines)
+    true_eqs = list(filter(is_true_p2, eqs))
+    print(true_eqs)
+
+    return sum([eq[0] for eq in true_eqs])
 
 
 def p1(lines: list[str]) -> int:
     eqs = parse(lines)
-#    print(eqs)
-#    print(list(ops(3)))
-    true_eqs = list(filter(is_true, eqs))
+    true_eqs = list(filter(is_true_p1, eqs))
 #    print(true_eqs)
 
     return sum([eq[0] for eq in true_eqs])
@@ -48,3 +69,4 @@ if __name__ == "__main__":
     d = Day(7)
     lines = d.read_lines()
     print(p1(lines))
+    print(p2(lines))
