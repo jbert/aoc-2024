c7317b59c29de7088c13f82516aa28ad911badac
John Berthels <john.berthels@gmail.com>
2024-12-20 15:26:22 +0000

    fast - precalc paths from start and paths to end from all spaces

--- d20.py
+++ d20.py
@@ -2,6 +2,7 @@ from day import Day
 from pt import pt, map_find
 from astar import find_path
 from typing import Iterable
+from time import time
 
 
 def maze_solve(spaces: set[pt], start: pt, end: pt) -> Iterable[pt] | None:
@@ -50,35 +51,74 @@ def display_cheat(lines: list[str], cheat):
         print(pline)
 
 
+def calc_paths_from(spaces, end) -> dict[pt, list[pt] | None]:
+    paths_from: dict[pt, list[pt] | None] = {}
+
+    # set of adjacent new and done pts
+    def find_adj(p: pt) -> tuple[set[pt], set[pt]]:
+        adj_spaces = set(p.adjacent_pts_nesw()).intersection(spaces)
+        return adj_spaces - done, adj_spaces.intersection(done)
+
+    paths_from[end] = [end]
+    done = set([end])
+    fringe, wtf = find_adj(end)
+    if len(wtf) > 0:
+        raise RuntimeError('wtf')
+
+    while len(fringe) > 0:
+        p = fringe.pop()
+
+        fs, qs = find_adj(p)
+        fringe = fringe.union(fs)
+
+        paths = sorted([v for v in [paths_from[q]
+                                    for q in qs] if v is not None], key=lambda l: len(l))
+        if len(paths) == 0:
+            paths_from[p] = None
+        else:
+            paths_from[p] = [p] + paths[0]
+        done.add(p)
+
+    return paths_from
+
+
 # 762 - too low
 def p1(lines: list[str]) -> int:
     spaces, walls, start, end = parse(lines)
-    path = maze_solve(spaces, start, end)
-    if path is None:
+
+    print('Calculating paths...')
+    before = time()
+    paths_from = calc_paths_from(spaces, end)
+    paths_to = calc_paths_from(spaces, start)
+    for path_to in paths_to.values():
+        if path_to is not None:
+            path_to.reverse()
+    after = time()
+    print(f'Done {after-before}')
+
+    start_to_end = paths_from[start]
+    if start_to_end is None:
         raise RuntimeError(f'Problem not solvable')
-    normal_picos = len(list(path))
+
+    normal_picos = len(start_to_end)
     print(start, end, normal_picos)
     count = 0
     num_checked = 0
     poss_cheats = list(possible_cheats(spaces, walls))
     lpc = len(poss_cheats)
-    for poss_cheat in poss_cheats:
-        print(f'pc {num_checked}/{lpc} {poss_cheat}')
+    for pc in poss_cheats:
         num_checked += 1
+#        print(f'pc {num_checked}/{lpc} {pc}')
 
-        spaces.add(poss_cheat[0])
-
-        ptc = maze_solve(spaces, start, poss_cheat[0])
+        ptcs = sorted(filter(lambda v: v is not None, [
+            paths_to[p] for p in pc[0].adjacent_pts_nesw() if p in spaces]), key=lambda l: len(l))
+        ptc = ptcs[0]
         if ptc is None:
             raise RuntimeError(f"can't path to cheat start")
-        ptc = list(ptc)
 
-        pfc = maze_solve(spaces, poss_cheat[1], end)
+        pfc = paths_from[pc[1]]
         if pfc is None:
             raise RuntimeError(f"can't path from cheat to end")
-        pfc = list(pfc)
-
-        spaces.remove(poss_cheat[0])
 
         if len(set(ptc).intersection(set(pfc))) != 0:
             continue
@@ -86,12 +126,16 @@ def p1(lines: list[str]) -> int:
         saved_picos = normal_picos - picos
         if saved_picos <= 0:
             continue
+#        print(f'pc {pc} picos {picos} saved {saved_picos}')
+#        print(f'ptc {ptc}')
+#        print(f'pfc {pfc}')
+#        display_cheat(lines, pc)
         if saved_picos >= 100:
             count += 1
-#            print(f'pc {poss_cheat} picos {picos} saved {saved_picos}')
+#            print(f'pc {pc} picos {picos} saved {saved_picos}')
 #            print(f'ptc {ptc}')
 #            print(f'pfc {pfc}')
-#            display_cheat(lines, poss_cheat)
+#            display_cheat(lines, pc)
     return count
 
 
